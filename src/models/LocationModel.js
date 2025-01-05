942591370eb9788e528e8dcd4f97fd1746d8915f
ashikur <ashikur1602@gmail.com>
2025-01-05 14:44:40 +0600

    location api updated

--- src/models/LocationModel.js
+++ src/models/LocationModel.js
@@ -2,15 +2,14 @@ const { default: mongoose } = require("mongoose");
 
 const DataSchema = mongoose.Schema(
     {
-        location: { type: String },
-        img: {type: String},
-
+        location: { type: String, unique: true, required: true },
+        img: { type: String },
     },
     {
         timestamps: true,
         versionKey: false
     }
-)
+);
 
 const LocationModel = mongoose.model('locations', DataSchema);
 module.exports = LocationModel;
