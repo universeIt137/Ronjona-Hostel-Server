b6450c5dbeefc3e68566c6ab7fa6c68812d8565c
ishanrana363 <ishanrana363@gmail.com>
2025-01-21 12:39:57 +0600

    find all key features api created

--- src/routes/api.js
+++ src/routes/api.js
@@ -19,7 +19,7 @@ const { createTeam, getAllTeamMember, getTeamMemberById, updateTeam, deleteTeam 
 const { createBooking } = require('../controllers/BookingController');
 const { whyChooseDataUpload, chooseDataById } = require('../controllers/WhyChooseController');
 const { missionVisionUpload, missionvissionById } = require('../controllers/missionVissionController');
-const { createKeyFeature } = require('../controllers/KeyFeatureController');
+const { createKeyFeature, getAllKeyFeatures } = require('../controllers/KeyFeatureController');
 
 
 const router = express.Router();
@@ -163,7 +163,8 @@ router.get("/vission-mission", missionvissionById);
 // key features api
 
 
-router.post("/key-features", createKeyFeature )
+router.post("/key-features", createKeyFeature);
+router.get("/key-features", getAllKeyFeatures);
 
 
 
