d7ca73f012d50dc384ef877b93755716091183d2
ishanrana363 <ishanrana363@gmail.com>
2025-01-28 17:06:05 +0600

    added

--- src/controllers/BookingController.js
+++ src/controllers/BookingController.js
@@ -22,6 +22,8 @@ exports.createBooking = async (req, res) => {
 exports.manageBookingPackages = async (req, res) => {
     try {
 
+        // join packages model
+
         const joinWithPakagesModel = {
             $lookup: {
                 from: "packages", // Collection you want to join with
@@ -31,12 +33,46 @@ exports.manageBookingPackages = async (req, res) => {
             }
         };
 
+        // join branch model
+
+        const joinWithBranchId = {
+            $lookup: {
+                from: "branches",
+                localField: "packageDetails.branch",
+                foreignField: "_id",
+                as: "branchDetails"
+            }
+        }
+
+        // join  location model
+
+        const joinWithLocationModel = {
+            $lookup: {
+                from: "locations",
+                localField: "branchDetails.location",
+                foreignField: "_id",
+                as: "locationDetails"
+            }
+        }
+
         // unwind
 
         const packagesUnwind = {
             $unwind: "$packageDetails" // Correctly specify the field path with $
         };
-        const data = await bookingModel.aggregate([joinWithPakagesModel, packagesUnwind])
+        // unwind branchDetails
+        const branchUnwind = {
+            $unwind: "$branchDetails" // Correctly specify the field path with $
+        };
+        // unwind location
+
+        const locationUnwind = {
+            $unwind: "$locationDetails" // Correctly specify the field path with $
+        };
+
+
+
+        const data = await bookingModel.aggregate([joinWithPakagesModel, packagesUnwind, joinWithBranchId, branchUnwind, joinWithLocationModel, locationUnwind])
 
         return successResponse(res, 200, "Data fetched successfully", data);
     } catch (error) {
