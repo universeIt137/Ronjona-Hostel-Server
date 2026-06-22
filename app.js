c7047e50343b1aed70ebb4d50d3c58640ada0462
universe it institute <universeitinstitute02@gmail.com>
2026-06-22 11:06:41 +0600

    fix get data issues

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const router = require("./src/routes/api");
-require("dotenv").config()
+require("dotenv").config();
 
 const app = new express();
 
@@ -15,10 +15,15 @@ app.use(express.json());
 
 const dbUrl = process.env.DB_URL;
 
+if (!dbUrl) {
+    console.error("---Database connect fail: DB_URL is missing---");
+    process.exit(1);
+}
+
 mongoose.connect(dbUrl).then((res) => {
     console.log("---Database connected---")
 }).catch((e) => {
-    console.log("---Database connect fail");
+    console.error("---Database connect fail---", e.message);
 })
 
 app.use('/api/v1',router);
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 require("dotenv").config();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 
